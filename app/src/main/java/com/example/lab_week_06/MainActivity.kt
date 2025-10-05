313d11944ce40d9777ba419a0ed53bf8ab64dbb1
theofilius <theofilius47@gmail.com>
2025-10-05 23:33:40 +0700

    ASSIGNMENT

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -37,10 +37,17 @@ class MainActivity : AppCompatActivity() {
 
         catAdapter.setData(
             listOf(
-                CatModel(Gender.Male,   CatBreed.BalineseJavanese, "Fred",  "Silent and deadly",        "https://cdn2.thecatapi.com/images/7dj.jpg"),
-                CatModel(Gender.Female, CatBreed.ExoticShorthair,  "Wilma", "Cuddly assassin",          "https://cdn2.thecatapi.com/images/egv.jpg"),
-                CatModel(Gender.Unknown,CatBreed.AmericanCurl,     "Curious George","Award winning investigator","https://cdn2.thecatapi.com/images/bar.jpg")
-            )
+                CatModel(Gender.Male,   CatBreed.BalineseJavanese, "Fred",   "Silent and deadly",          "https://cdn2.thecatapi.com/images/7dj.jpg"),
+                CatModel(Gender.Female, CatBreed.ExoticShorthair,  "Wilma",  "Cuddly assassin",            "https://cdn2.thecatapi.com/images/egv.jpg"),
+                CatModel(Gender.Unknown,CatBreed.AmericanCurl,     "Curious George","Award winning investigator","https://cdn2.thecatapi.com/images/bar.jpg"),
+                CatModel(Gender.Male,   CatBreed.AmericanCurl,     "Ollie",  "Window-sill philosopher",    "https://cdn2.thecatapi.com/images/8ap.jpg"),
+                CatModel(Gender.Female, CatBreed.BalineseJavanese, "Mochi",  "Zoomies at 3 AM",            "https://cdn2.thecatapi.com/images/2oo.jpg"),
+                CatModel(Gender.Male,   CatBreed.ExoticShorthair,  "Simba",  "Nap enthusiast",             "https://cdn2.thecatapi.com/images/9je.jpg"),
+                CatModel(Gender.Female, CatBreed.AmericanCurl,     "Nala",   "Laser pointer hunter",       "https://cdn2.thecatapi.com/images/aei.jpg"),
+                CatModel(Gender.Unknown,CatBreed.ExoticShorthair,  "Pudding","Head-bump specialist",       "https://cdn2.thecatapi.com/images/ccv.jpg"),
+                CatModel(Gender.Male,   CatBreed.BalineseJavanese, "Biscuit","Sunbeam sleeper",            "https://cdn2.thecatapi.com/images/b1o.jpg"),
+                CatModel(Gender.Female, CatBreed.AmericanCurl,     "Cleo",   "Bird watcher extraordinaire","https://cdn2.thecatapi.com/images/3tb.jpg")
+         )
         )
     }
 
